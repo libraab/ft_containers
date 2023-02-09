5e226ca1b5c50534b6a9cafa17fd9da31363db6a
libraab <asmabouhlelboughdira@hotmail.com>
2023-02-09 18:01:25 +0100

    adding member types

--- Containers/vector.hpp
+++ Containers/vector.hpp
@@ -5,7 +5,6 @@
 
 namespace ft
 {
-
 	template <class T, class Alloc = std::allocator<T> >
     class vector
     {
@@ -20,18 +19,18 @@ namespace ft
             //=========================================//
             // -------- M E M B E R   T Y P E ---------//
             //=========================================//
-            typedef T	                                        value_type;
-            typedef Alloc	                                    allocator_type;
-            typedef typename allocator_type::reference	        reference;
-            typedef typename allocator_type::const_reference    const_reference;
-            typedef typename allocator_type::pointer	        pointer;
-            typedef typename allocator_type::const_pointer	    const_pointer;
-            typedef typename allocator_type::size_type          size_type;
-            // typedef iterator;
-            // typedef	const_iterator;
-            // typedef	reverse_iterator;
-            // typedef	const_reverse_iterator;
-            // typedef typename iterator_traits<iterator>::difference_type	    difference_type;
+            typedef T	                                                        value_type;
+            typedef Alloc	                                                    allocator_type;
+            typedef value_type&	                                                reference;
+            typedef const value_type&                                           const_reference;
+            typedef typename allocator_type::pointer	                        pointer;
+            typedef typename allocator_type::const_pointer	                    const_pointer;
+            typedef size_t                                                      size_type;
+            typedef std::ptrdiff_t	                                            difference_type;
+            typedef ft::iterator<value_type>                                    iterator;
+            typedef ft::iterator<const value_type>                              const_iterator;
+            typedef	reverse_iterator;
+            typedef	const_reverse_iterator;
 
             //=========================================//
             // ----- M E M B E R   F U N C TI O N -----//
