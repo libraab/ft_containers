d49e10963b962df942657535c145976a21888437
libraab <asmabouhlelboughdira@hotmail.com>
2023-02-10 15:17:08 +0100

    stack is DONE

--- Containers/stack.hpp
+++ Containers/stack.hpp
@@ -2,82 +2,63 @@
 #include "../ft_containers.hpp"
 #include "vector.hpp"
 
+ // --> https://cplusplus.com/reference/stack/stack/
 namespace ft
 {
-  // --> https://cplusplus.com/reference/stack/stack/
-    template<class T, class container = std::vector<T> >
-    class stack
+	template<class T, class container = std::vector<T> >
+	class stack
     {
-      public:
-    //========================================================================//
-    //                    M E M B E R          T Y P E S                      //
-    //========================================================================//
-        typedef T           value_type;
-        typedef container   container_type;
-    //========================================================================//
-    //                        C O N S T R U C T O R                           //
-    //========================================================================//
-    // --> https://cplusplus.com/reference/stack/stack/stack/
-    explicit stack (const container_type& ctnr = container_type()) { _ctnr(ctnr)}
-    //========================================================================//
-    //             R E L A T I O N A L          O P E R A T O R S             //
-    //========================================================================//
-    // --> https://cplusplus.com/reference/stack/stack/operators/
-    template <class T, class Container>
-    friend bool operator== (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
-    {return (lhs._ctnr == rhs._ctnr);}
-    
-    template <class T, class Container>
-    friend bool operator!= (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
-    {return (lhs._ctnr != rhs._ctnr);}
+    	public:
+        //====================================================================//
+        //                    M E M B E R          T Y P E S                  //
+        //====================================================================//
+        	typedef T           value_type;
+        	typedef container   container_type;
+        //====================================================================//
+        //                        C O N S T R U C T O R                       //
+        //====================================================================//
+        // --> https://cplusplus.com/reference/stack/stack/stack/
+        	explicit stack (const container_type& ctnr = container_type()) { _ctnr(ctnr)}
+        //====================================================================//
+        //           R E L A T I O N A L          O P E R A T O R S           //
+        //====================================================================//
+        // --> https://cplusplus.com/reference/stack/stack/operators/
+			template <class T, class Container>
+			friend bool operator== (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+			{return (lhs._ctnr == rhs._ctnr);}
+			
+			template <class T, class Container>
+			friend bool operator!= (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+			{return (lhs._ctnr != rhs._ctnr);}
 
-    template <class T, class Container>
-    friend bool operator<  (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
-    {return (lhs._ctnr < rhs._ctnr);}
+			template <class T, class Container>
+			friend bool operator<  (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+			{return (lhs._ctnr < rhs._ctnr);}
 
-    template <class T, class Container> 
-    friend bool operator<= (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
-    {return (lhs._ctnr <= rhs._ctnr);}
+			template <class T, class Container> 
+			friend bool operator<= (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+			{return (lhs._ctnr <= rhs._ctnr);}
 
-    template <class T, class Container> 
-    friend bool operator>  (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
-    {return (lhs._ctnr > rhs._ctnr);}
+			template <class T, class Container> 
+			friend bool operator>  (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+			{return (lhs._ctnr > rhs._ctnr);}
 
-    template <class T, class Container> 
-    friend bool operator>= (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
-    {return (lhs._ctnr >= rhs._ctnr);}
-    //========================================================================//
-    //                    M E M B E R    F U N C T I O N S                    //
-    //========================================================================//
-    bool empty() const {retur}
-    size_t size() const;
-    value_type& top();const value_type& top() const;
-    void push (const value_type& val);
-
-    template <class... Args> void emplace (Args&&... args);
-    void pop();
-    void swap (stack& x) noexcept(/*see below*/);
-
+			template <class T, class Container> 
+			friend bool operator>= (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
+			{return (lhs._ctnr >= rhs._ctnr);}
+        //====================================================================//
+        //                  M E M B E R    F U N C T I O N S                  //
+        //====================================================================//
+			bool empty() const                  {return (_ctnr.empty());}
+			size_t size() const                 {return (_ctnr.size());}
+			value_type& top()                   {return (_ctnr.back());}
+			const value_type& top() const       {return (_ctnr.push_back());}
+			void push (const value_type& val)   {return (_ctnr.push_back());}
     //--------------------------------------------------------------------------
-      protected:
-        container_type      _ctnr;
+    	protected:
+        	container_type      _ctnr;
   };
 }
-// ❌ ✅
-/* M E M B E R - T Y P E S
-    - value_type ❌
-    - container_type ❌
-    - size_type ❌
-   M E M B E R - F U N C T I O N S
-    - Constructor ❌
-    - empty ❌
-    - size ❌
-    - top ❌
-    - push ❌
-    - emplace ❌
-    - pop ❌
-    - swap ❌
-  N O N - M E M B E R - F U N C T I O N 
-    - relational operators ❌
-    - swap (stack) ❌
-*/
+// typedef -> give new name to existing data type
+// explicit -> only direct initialisation (no implicit conversion or copy initialisation)
+// friend -> can acess private & protected stuff from class 
