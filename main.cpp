1d813c0c358d3b79cddd14173e9d33144baa3cfa
libraab <asmabouhlelboughdira@hotmail.com>
2023-02-13 09:41:24 +0100

    fixing stack's errors with tester

--- main.cpp
+++ main.cpp
@@ -1,3 +1,30 @@
 #include "stack.hpp"
 #include <iostream>
 
+int main() {
+  ft::stack<int> myStack;
+
+  // Push elements onto the stack
+  myStack.push(1);
+  myStack.push(2);
+  myStack.push(3);
+
+  // Display the top element of the stack
+  std::cout << "Top element: " << myStack.top() << std::endl;
+
+  // Pop elements from the stack
+  myStack.pop();
+  std::cout << "Top element after pop: " << myStack.top() << std::endl;
+  
+  // Check if the stack is empty
+  if (myStack.empty()) {
+    std::cout << "The stack is empty" << std::endl;
+  } else {
+    std::cout << "The stack is not empty" << std::endl;
+  }
+
+  return 0;
+}
+
+
+
--- stack.hpp
+++ stack.hpp
@@ -1,12 +1,11 @@
 #pragma once
 #include "ft_containers.hpp"
 #include "vector.hpp"
-#include<vector>
 
  // --> https://cplusplus.com/reference/stack/stack/
 namespace ft
 {
-	template<class T, class container = std::vector<T> >
+	template<class T, class container = ft::vector<T> >
 	class stack
     {
     	public:
@@ -19,42 +18,45 @@ namespace ft
         //                        C O N S T R U C T O R                       //
         //====================================================================//
         // --> https://cplusplus.com/reference/stack/stack/stack/
-        	explicit stack (const container_type& ctnr = container_type()) { _ctnr(ctnr)}
+        explicit stack (const container_type& ctnr = container_type()) { _ctnr = ctnr;}
         //====================================================================//
         //           R E L A T I O N A L          O P E R A T O R S           //
         //====================================================================//
         // --> https://cplusplus.com/reference/stack/stack/operators/
-			template <class T, class Container>
+			template <class TT, class Container>
 			friend bool operator== (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
 			{return (lhs._ctnr == rhs._ctnr);}
 			
-			template <class T, class Container>
+			template <class TT, class Container>
 			friend bool operator!= (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
 			{return (lhs._ctnr != rhs._ctnr);}
 
-			template <class T, class Container>
+			template <class TT, class Container>
 			friend bool operator<  (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
 			{return (lhs._ctnr < rhs._ctnr);}
 
-			template <class T, class Container> 
+			template <class TT, class Container> 
 			friend bool operator<= (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
 			{return (lhs._ctnr <= rhs._ctnr);}
 
-			template <class T, class Container> 
+			template <class TT, class Container> 
 			friend bool operator>  (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
 			{return (lhs._ctnr > rhs._ctnr);}
 
-			template <class T, class Container> 
+			template <class TT, class Container> 
 			friend bool operator>= (const stack<T,Container>& lhs, const stack<T,Container>& rhs)
 			{return (lhs._ctnr >= rhs._ctnr);}
         //====================================================================//
         //                  M E M B E R    F U N C T I O N S                  //
         //====================================================================//
+		// --> https://en.cppreference.com/w/cpp/container/stack
+		// --> https://legacy.cplusplus.com/reference/stack/stack/
 			bool empty() const                  {return (_ctnr.empty());}
 			size_t size() const                 {return (_ctnr.size());}
 			value_type& top()                   {return (_ctnr.back());}
-			const value_type& top() const       {return (_ctnr.push_back());}
-			void push (const value_type& val)   {return (_ctnr.push_back());}
+			const value_type& top() const       {return (_ctnr.back());}
+			void push (const value_type& val)   {return (_ctnr.push_back(val));}
+			void pop()							{return (_ctnr.pop_back());}
     //--------------------------------------------------------------------------
     	protected:
         	container_type      _ctnr;
