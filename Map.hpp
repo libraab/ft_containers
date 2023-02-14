319cd724c0409865fa505eb71c47b2efcebf1bef
libraab <asmabouhlelboughdira@hotmail.com>
2023-02-14 09:45:14 +0100

    switching to school pc

--- Map.hpp
+++ Map.hpp
@@ -45,32 +45,38 @@ namespace ft
         //====================================================================//
         public:
             // --> https://legacy.cplusplus.com/reference/map/map/map/
-            //empty 
-            explicit map (const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type());
-            //range
+            //empty (2)
+            explicit map (const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type())
+            {_comp = comp;
+            allocator_type = alloc;}
+            //range (4)
             template <class InputIterator> 
-            map (InputIterator first, InputIterator last, const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type());
-            //copy
+            map (InputIterator first, InputIterator last, const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type())
+            {
+                _comp = comp;
+                allocator_type = alloc;
+                // insert iterator into node 
+            }
+            //copy (1)
             map (const map& x);
             //destructor
             ~map();
             //-----------------------------------------------------------------
             // V A L U E _ C O M P A R E 
             // --> https://cplusplus.com/reference/map/map/value_comp/
-            template <class Key, class T, class Compare, class Alloc>
-            class map<Key,T,Compare,Alloc>::value_compare
+            class value_compare
             {   // in C++98, it is required to inherit binary_function<value_type,value_type,bool>
                 friend class map;
                 protected:
-                Compare comp;
-                value_compare (Compare c) : comp(c) {}  // constructed with map's comparison object
+                    Compare comp;
+                    value_compare (Compare c) : comp(c) {}  // constructed with map's comparison object
                 public:
-                typedef bool result_type;
-                typedef value_type first_argument_type;
-                typedef value_type second_argument_type;
-                bool operator() (const value_type& x, const value_type& y) const
-                {return comp(x.first, y.first);}
-            }
+                    typedef bool result_type;
+                    typedef value_type first_argument_type;
+                    typedef value_type second_argument_type;
+                    bool operator() (const value_type& x, const value_type& y) const
+                        return comp(x.first, y.first);
+            };
             //------------------------------------------------------------------
             map& operator= (const map& x) {
                 clear();
